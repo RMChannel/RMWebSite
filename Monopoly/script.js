09570d44291ba9de91a51c66d4573e4899b6bbdd
RMChannel <98057423+RMChannel@users.noreply.github.com>
2022-08-06 17:38:27 +0200

    Funziona potenziare proprietà implementata, ancora da termianre

--- Monopoly/script.js
+++ Monopoly/script.js
@@ -9,16 +9,18 @@ const error= new Audio('sounds/error.mp3');
 const press= new Audio('sounds/presssound.mp3');
 const cash= new Audio('sounds/cash.mp3');
 const lost= new Audio('sounds/lost.mp3');
-const name = [ "Vicolo Stretto", "Vicolo Corto","Stazione SUD","Bastioni Gran Sasso","Viale Monterosa","Viale Vesuvio","Via Accademia","Societa' Elettrica","Corso Ateneo","Piazza Universita'","Stazione OVEST","Via Verdi","Corso Raffaello","Piazza Dante","Via Marcolo Polo","Corso Magellano","Largo Colombo","Stazione NORD","Viale Costantino","Viale Traiano","Societa' Acqua Potabile","Piazza Giulio Cesare","Via Roma","Corso Impero","Largo Augusto","Stazione EST","Viale Dei Giardini","Parco della Vittoria" ];
 const price=[ 60,60,100,100,120,140,140,160,180,180,200,220,220,240,260,260,280,300,300,320,350,400,150,150,200,200,200,200 ];
+const casavalore = [ 50,50,50,50,50,100,100,100,100,100,100,150,150,150,150,150,150,200,200,200,200,200 ];
 let proprieta= [];
+let casaproprieta= [];
+let lvlcasa= [];
 
 function playerassing() {
     press.play();
     p=document.getElementById("numberinsert").value;
     if (p<2 || p>4) {
         error.play();
-        alert("Errore:1\nNumero giocatori inserito incorretto, riprovare");
+        alert("Errore: 1\nNumero giocatori inserito incorretto, riprovare");
     }
     else {
         game();
@@ -38,7 +40,8 @@ function reloadplayers() {
         document.getElementById("via3").style.display="inline-block";
         document.getElementById("tassep3").style.display="inline-block";
         document.getElementById("acquisto3").style.display="inline-block";
-        document.getElementById("visu3").style.display="inline-block"; 
+        document.getElementById("visu3").style.display="inline-block";
+        document.getElementById("pc3").style.display="inline-block";
         p3.innerText="P3:"+p3s+"€";
     }
     if (p==4) {
@@ -46,13 +49,14 @@ function reloadplayers() {
         document.getElementById("via4").style.display="inline-block";
         document.getElementById("tassep4").style.display="inline-block";
         document.getElementById("acquisto4").style.display="inline-block"; 
-        document.getElementById("visu4").style.display="inline-block";       
+        document.getElementById("visu4").style.display="inline-block";  
+        document.getElementById("pc4").style.display="inline-block";       
         p4.innerText="P4:"+p4s+"€";
     }
 }
 
 function hideall() {
-    document.getElementById("functions").style.display="block";
+    document.getElementById("functions").style.display="inline";
     document.getElementById("Via").style.display="none";
     document.getElementById("tassemenu1").style.display="none";
     document.getElementById("tassemenu2").style.display="none";
@@ -62,17 +66,15 @@ function hideall() {
     document.getElementById("visualmenu1").style.display="none";
     document.getElementById("visualmenu2").style.display="none";
     document.getElementById("exitvis").style.display="none";
+    document.getElementById("potenziacasa1").style.display="none";
+    document.getElementById("potenziacasa2").style.display="none";
+    document.getElementById("exitpote").style.display="none";
+    document.getElementById("potenziacasa3").style.display="none";
     let i=0;
     while (i<28) {
         let temp=i.toString();
         document.getElementById(temp).style.display="none";
-        i++;
-    }
-    i=0;
-    while (i<28) {
-        let temp=i.toString();
         temp+="p";
-        console.log(temp);
         document.getElementById(temp).style.display="none";
         i++;
     }
@@ -235,7 +237,6 @@ function propshow() {
 
 function confermaprop() {
     press.play();
-    console.log(ppa);
     let temp=ppa.toString();
     document.getElementById("acquistomenu2").style.display="none";
     document.getElementById("acquistomenu3").style.display="flex";
@@ -332,7 +333,128 @@ function visu2() {
     }
     if (check==false) {
         error.play();
-        alert("Errore2\nNon hai nessuna proprietà");
+        alert("Errore: 2\nNon hai nessuna proprietà");
+        reset();
+    }
+}
+
+function potenziactive() {
+    press.play();
+    hideall();
+    document.getElementById("functions").style.display="none";
+    document.getElementById("potenziacasa1").style.display="block";
+}
+
+function potenziaselect() {
+
+}
+
+function controllo() {
+    let check=false;
+    if ((ta==0) || (ta==1)) {
+        if ((proprieta[0]==pa) && (proprieta[1]==pa)) {
+            check=true;
+        }
+    }
+    else if ((ta==2) || (ta==3) || (ta==4)) {
+        if ((proprieta[2]==pa) && (proprieta[3]==pa) && (proprieta[4]==pa)) {
+            check=true;
+        }
+    }
+    else if ((ta==5) || (ta==6) || (ta==7)) {
+        if ((proprieta[5]==pa) && (proprieta[6]==pa) && (proprieta[7]==pa)) {
+            check=true;
+        }
+    }
+    else if ((ta==8) || (ta==9) || (ta==10)) {
+        if ((proprieta[8]==pa) && (proprieta[9]==pa) && (proprieta[10]==pa)) {
+            check=true;
+        }
+    }
+    else if ((ta==11) || (ta==12) || (ta==13)) {
+        if ((proprieta[11]==pa) && (proprieta[12]==pa) && (proprieta[13]==pa)) {
+            check=true;
+        }
+    }
+    else if ((ta==14) || (ta==15) || (ta==16)) {
+        if ((proprieta[14]==pa) && (proprieta[15]==pa) && (proprieta[16]==pa)) {
+            check=true;
+        }
+    }
+    else if ((ta==17) || (ta==18) || (ta==19)) {
+        if ((proprieta[17]==pa) && (proprieta[18]==pa) && (proprieta[19]==pa)) {
+            check=true;
+        }
+    }
+    else if ((ta==20) || (ta==21)) {
+        if ((proprieta[20]==pa) && (proprieta[21]==pa)) {
+            check=true;
+        }
+    }
+    if (check==false) {
+        error.play();
+        alert("Errore: 3\nNon hai un gruppo di proprietà adatto o la proprietà selezionata non è potenziabile");
+    }
+    else {
+        document.getElementById("potenziacasa2").style.display="none";
+        document.getElementById("potenziacasa3").style.display="block";
+    }
+}
+
+function potenziacasa() {
+    press.play();
+    let check=false;
+    let i=0;
+    document.getElementById("potenziacasa1").style.display="none";
+    document.getElementById("potenziacasa2").style.display="block";
+    document.getElementById("exitpote").style.display="block";
+    if (pa==1) {
+        while (i<28) {
+            if (proprieta[i]==1) {
+                check=true;
+                let temp=i.toString();
+                temp+="pa";
+                document.getElementById(temp).style.display="inline";
+            }
+            i++;
+        }
+    }
+    else if (pa==2) {
+        while (i<28) {
+            if (proprieta[i]==2) {
+                check=true;
+                let temp=i.toString();
+                temp+="pa";
+                document.getElementById(temp).style.display="inline";
+            }
+            i++;
+        }
+    }
+    else if (pa==3) {
+        while (i<28) {
+            if (proprieta[i]==3) {
+                check=true;
+                let temp=i.toString();
+                temp+="pa";
+                document.getElementById(temp).style.display="inline";
+            }
+            i++;
+        }
+    }
+    else {
+        while (i<28) {
+            if (proprieta[i]==4) {
+                check=true;
+                let temp=i.toString();
+                temp+="pa";
+                document.getElementById(temp).style.display="inline";
+            }
+            i++;
+        }
+    }
+    if (check==false) {
+        error.play();
+        alert("Errore: 4\nNon hai nessuna proprietà");
         reset();
     }
 }
@@ -341,10 +463,13 @@ function game() {
     let i=0;
     while (i<28) {
         proprieta[i]=0;
+        casaproprieta[i]=0;
+        lvlcasa[i]=0;
         i++;
     }
     document.getElementById("selplayer").style.display="none";
     let game=true;
     document.getElementById("menu").style.display="block";
     reloadplayers();
+    hideall();
 }
