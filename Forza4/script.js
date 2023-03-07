29c34b0839084c9a012385a67b85ccb4d1503bb4
RMChannel <98057423+RMChannel@users.noreply.github.com>
2023-03-07 22:23:43 +0100

    Forza 4 completato
    SI ci ho messo un bel po' XD

--- Forza4/script.js
+++ Forza4/script.js
@@ -1,71 +1,314 @@
 const cells=document.querySelectorAll('.cell');
-let turn=0;
-let win=0;
-let temp=0;
-let loop=false;
+const winsound=new Audio("win.mp3")
+let firstrun=false
+let turn=0
+game=true
+vic=document.getElementById("victory")
 const cellcontrol=[];
 for (let i=0; i<cells.length; i++) {
-    let cell=cells[i];
+    let cell=cells[i]
+    cellcontrol.push("n")
     cell.addEventListener('click', function() {
-        loop=false;
-        cell=cells[i];
-        console.log(i);
-        if (loop==false) {
-            while (loop==false) {
-                console.log(i);
-                console.log(loop);
-                if (cellcontrol[i]) {
-                    i=i-7;
-                    loop=true;
-                }
-                else if (i>34) {
-                    loop=true;
+        if (game) {
+            input=i
+            if (firstrun) {
+                clearInterval(animation)
+                cell1.style.color=color+"1.0)"
+            }
+            cellfunct()
+            if (controlvictory()) {
+                winsound.play()
+                string="Il giocatore "
+                if (winner=="r") {
+                    string+="Rosso"
+                    vic.style.color="rgba(255,0,0,1)"
                 }
                 else {
-                    i=i+7;
+                    string+="Giallo"
+                    vic.style.color="rgba(255,200,0,1)"
+                }
+                string+=" ha vinto!"
+                
+                vic.innerHTML=string
+                game=false
+            }
+        }
+    })    
+}
+
+
+
+
+var cell1
+var color
+function cellfunct() {
+    console.log(input);
+    error=false
+    firstrun=true
+    while (input<35) {
+        input+=7
+    }
+    control=true
+    while (control) {
+        if (cellcontrol[input]=="n") control=false
+        else if (input<7 && cellcontrol[input]!="n") {
+            alert("Errore")
+            error=true
+            break;
+        }
+        else {
+            input-=7
+        }
+    }
+    if (turn==0 && !error) {
+        cellcontrol[input]="r"
+        turn=1
+        color="rgba(255,0,0,"
+    }
+    else if (turn==1 && !error) {
+        cellcontrol[input]="y"
+        turn=0
+        color="rgba(255,200,0,"
+    }
+    cell1=cells[input]
+    cell1.innerText="0"
+    let i=0
+    animation=setInterval(() => {
+        if (i==10) {
+            cell1.style.color=color+"1.0)"
+            clearInterval(animation)
+        }
+        else {
+            cell1.style.color=color+"0."+i+")"
+        }
+        i+=1
+    }, 100);
+}
+
+var winner
+function controlvictory() {
+    //Controllo Orizzontale
+    i=0
+    while (i<42) {
+        control=0
+        temp="n"
+        for (i2=0; i2<7; i2++) {
+            if (cellcontrol[i]!="n") {
+                if (temp==cellcontrol[i]) control++
+                else {
+                    control=1
+                    temp=cellcontrol[i]
                 }
             }
+            else {
+                if (temp="r") {
+                }
+                control=0
+            }
+            if (control==4) {
+                winner=temp
+                return true
+            }
+            i+=1
         }
-        cell=cells[i];
-        if (loop==true) {
-            if (temp==0) {
-                cell.style.color="red";
-                cellcontrol[i]='r';
-                temp=1;
+    }
+    //Controllo Verticale
+    i=0
+    while (i<7) {
+        control=1
+        temp="n"
+        for (i2=i; i2<42; i2+=7) {
+            if (cellcontrol[i2]!="n") {
+                if (temp==cellcontrol[i2]) control++
+                else {
+                    control=1
+                    temp=cellcontrol[i2]
+                }
+            }
+            if (control==4) {
+                winner=temp
+                return true
+            }
+        }
+        i+=1
+    }
+    //Controllo obliquo da basso-sinistra a alto-destra
+    if (((cellcontrol[14]=="r") || (cellcontrol[14]=="y")) && (cellcontrol[14]==cellcontrol[22]) && (cellcontrol[22]==cellcontrol[30]) && (cellcontrol[30]==cellcontrol[38])) {
+        winner=cellcontrol[14]
+        return true
+    }
+    if (((cellcontrol[3]=="r") || (cellcontrol[3]=="y")) && (cellcontrol[3]==cellcontrol[11]) && (cellcontrol[11]==cellcontrol[19]) && (cellcontrol[19]==cellcontrol[27])) {
+        winner=cellcontrol[3]
+        return true
+    }
+    control=1
+    temp="n"
+    for (i=0; i<41; i+=8) {
+        if (cellcontrol[i]!="n") {
+            if (temp==cellcontrol[i]) {
+                control++
             }
             else {
-                cell.style.color="yellow";
-                cellcontrol[i]='y';
-                temp=0;
-            } 
-            cell.innerText='O';
-            CheckVictory();
-            loop=false;
-            return;
-        }
-    });
-}
-
-function restart() {
-    location.reload();
-}
-/*
-function CheckVictory() {
-    let z=0;
-    let i2=0;
-    let i3=0;
-    let i4;
-    while (i3<7) {
-        if (cellcontrol[i2]) {
-            i4=cellcontrol[i2];
-            z++;
-            while (z>0) {
-
+                control=1
+                temp=cellcontrol[i]
             }
         }
         else {
-            i2++;
-            i3++;
+            control=1
+        }
+        if (control==4) {
+            winner=temp
+            return true
         }
     }
-}*/
+    control=1
+    temp="n"
+    for (i=7; i<41; i+=8) {
+        if (cellcontrol[i]!="n") {
+            if (temp==cellcontrol[i]) {
+                control++
+            }
+            else {
+                control=1
+                temp=cellcontrol[i]
+            }
+        }
+        else {
+            control=1
+        }
+        if (control==4) {
+            winner=temp
+            return true
+        }
+    }
+    control=1
+    temp="n"
+    for (i=1; i<42; i+=8) {
+        if (cellcontrol[i]!="n") {
+            if (temp==cellcontrol[i]) {
+                control++
+            }
+            else {
+                control=1
+                temp=cellcontrol[i]
+            }
+        }
+        else {
+            control=1
+        }
+        if (control==4) {
+            winner=temp
+            return true
+        }
+    }
+    control=1
+    temp="n"
+    for (i=2; i<41; i+=8) {
+        if (cellcontrol[i]!="n") {
+            if (temp==cellcontrol[i]) {
+                control++
+            }
+            else {
+                control=1
+                temp=cellcontrol[i]
+            }
+        }
+        else {
+            control=1
+        }
+        if (control==4) {
+            winner=temp
+            return true
+        }
+    }
+    //Controllo obliquo basso-destra a alto-sinistra
+    if (((cellcontrol[20]=="r") || (cellcontrol[20]=="y")) && (cellcontrol[20]==cellcontrol[26]) && (cellcontrol[26]==cellcontrol[32]) && (cellcontrol[32]==cellcontrol[38])) {
+        winner=cellcontrol[20]
+        return true
+    }
+    if (((cellcontrol[3]=="r") || (cellcontrol[3]=="y")) && (cellcontrol[3]==cellcontrol[9]) && (cellcontrol[9]==cellcontrol[15]) && (cellcontrol[15]==cellcontrol[21])) {
+        winner=cellcontrol[3]
+        return true
+    }
+    control=1
+    temp="n"
+    for (i=13; i<41; i+=6) {
+        if (cellcontrol[i]!="n") {
+            if (temp==cellcontrol[i]) {
+                control++
+            }
+            else {
+                control=1
+                temp=cellcontrol[i]
+            }
+        }
+        else {
+            control=1
+        }
+        if (control==4) {
+            winner=temp
+            return true
+        }
+    }
+    control=1
+    temp="n"
+    for (i=6; i<41; i+=6) {
+        if (cellcontrol[i]!="n") {
+            if (temp==cellcontrol[i]) {
+                control++
+            }
+            else {
+                control=1
+                temp=cellcontrol[i]
+            }
+        }
+        else {
+            control=1
+        }
+        if (control==4) {
+            winner=temp
+            return true
+        }
+    }
+    control=1
+    temp="n"
+    for (i=5; i<41; i+=6) {
+        if (cellcontrol[i]!="n") {
+            if (temp==cellcontrol[i]) {
+                control++
+            }
+            else {
+                control=1
+                temp=cellcontrol[i]
+            }
+        }
+        else {
+            control=1
+        }
+        if (control==4) {
+            winner=temp
+            return true
+        }
+    }
+    control=1
+    temp="n"
+    for (i=4; i<29; i+=6) {
+        if (cellcontrol[i]!="n") {
+            if (temp==cellcontrol[i]) {
+                control++
+            }
+            else {
+                control=1
+                temp=cellcontrol[i]
+            }
+        }
+        else {
+            control=1
+        }
+        if (control==4) {
+            winner=temp
+            return true
+        }
+    }
+    return false
+}
